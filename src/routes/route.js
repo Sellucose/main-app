eae5795dccb5413ee478a65f69887f52c0a99a4b
Your Name <infilol123@gmail.com>
2024-06-08 18:11:43 +0700

    testing

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,6 +7,7 @@ const authRoutes = require('./authRoute')
 const savedBookRoutes = require('./savedBookRoute');
 const recommendationsRoute = require('./recommendationsRoute');
 const profileRoute = require('./profileRoute');
+const rateBookRoute = require('./rateBookRoute');
 
 router.get('/main', authMiddleware, (req,res) => {
   res.json({ message: 'login only bos', user: req.user });
@@ -25,6 +26,10 @@ recommendationsRoute.forEach(route => {
 });
 
 profileRoute.forEach(route => {
+  router[route.method](route.path, route.middleware, route.controller);
+});
+
+rateBookRoute.forEach(route => {
   router[route.method](route.path, route.middleware, route.controller);
 });
 
